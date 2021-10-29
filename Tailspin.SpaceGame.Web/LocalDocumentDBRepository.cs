a5184189bebe0622ace41e91b39767c38f6b1d43
trainee0811 <divesh.gaikwad@tcs.com>
2021-10-29 12:09:08 +0000

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -35,20 +35,20 @@ namespace TailSpin.SpaceGame.Web
         /// </returns>
         /// <param name="id">The identifier of the item to retrieve.</param>
         public Task<IEnumerable<T>> GetItemsAsync(
-    Expression<Func<T, bool>> queryPredicate,
-    Expression<Func<T, int>> orderDescendingPredicate,
-    int page = 1, int pageSize = 10
-)
-{
-    var result = _items.AsQueryable()
-        .Where(queryPredicate) // filter
-        .OrderByDescending(orderDescendingPredicate) // sort
-        .Skip(page * pageSize) // find page
-        .Take(pageSize) // take items
-        .AsEnumerable(); // make enumeratable
+          Expression<Func<T, bool>> queryPredicate,
+          Expression<Func<T, int>> orderDescendingPredicate,
+          int page = 1, int pageSize = 10
+        )
+        {
+          var result = _items.AsQueryable()
+           .Where(queryPredicate) // filter
+           .OrderByDescending(orderDescendingPredicate) // sort
+           .Skip(page * pageSize) // find page
+           .Take(pageSize) // take items
+           .AsEnumerable(); // make enumeratable
 
-    return Task<IEnumerable<T>>.FromResult(result);
-}
+          return Task<IEnumerable<T>>.FromResult(result);
+        }
 
         /// <summary>
         /// Retrieves items from the store that match the given query predicate.
